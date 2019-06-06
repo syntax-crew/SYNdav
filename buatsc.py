c21d261f2d7db964da1508c46beea9d70b7d67fa
syntax-crew <syntaxerrorcrew.id@gmail.com>
2019-06-06 23:15:04 +0700

    Create buatsc.py

---
+++ buatsc.py
@@ -0,0 +1,125 @@
+#! /usr/bin/python
+#Hapus Copyright?Lo Bukan Coder Bro
+
+#masuk tools
+
+import os
+import sys
+
+os.system ('termux-setup-storage')
+os.system ('clear')
+
+syntax = """ ____   ____     _____       _
+/ ___| / ___|_ _|___ /  __ _| |_
+\___ \| |   | '__||_ \ / _` | __|
+ ___) | |___| |  ___) | (_| | |_
+|____/ \____|_| |____/ \__,_|\__|v.1"""
+
+print syntax
+
+print "(+)---------------------------ABOUT(+)"
+
+print " | TEAM    : Syntax Error Crew      |"
+
+print "(+)--------------------------------(+)"
+syntitle = raw_input(" | }{ploit@title : ")
+synteam = raw_input(" | }{ploit@team : ")
+synick = raw_input(" | }{ploit@nick : ")
+synfriend = raw_input(" | }{ploit@friend : ")
+os.system ('sleep 3')
+print "(+)----------------------------DONE(+)"
+os.system ('sleep 2')
+print " | SELESAI DIBUAT >>   |"
+print "(+)--------------------------------(+)"
+syn = open("syntaxcrew.html","w")
+syntax = """<html>
+	<head>
+		<title>"""
+syntax1 = syntitle
+syntax2 = """</title>
+		<meta content="Security Just Illusion" />
+
+		<meta name="title" content=" """
+syntax3 = synteam
+syntax4 = """ " />
+
+		<meta name="googlebot" content="index,follow" />
+
+		<meta name="robots" content="all" />
+
+		<meta name="robots schedule" content="auto" />
+
+		<meta name="distribution" content="global" />
+
+		<meta contact="#" />
+
+		<meta charset="utf-8" />
+
+		<style>html, body { color: #636b6f; font-family: 'Fredericka the Great', sans-serif; font-weight: 100; height: 100vh; margin: 0; } .full-height { height: 100vh; } .flex-center { align-items: center; display: flex; justify-content: center; } .position-ref { position: relative; } .content { text-align: center; } .title { font-size: 36px; padding: 20px; }</style>
+		<style type="text/css">* { margin:0; padding:0; }  div {  font-family: 'Merienda'; } html { background-color: #000000; font-family: Merienda; no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; } .text_d7net { font-weight:200px; font-size:20px; background:#000000; color:#3e6a06; text-shadow: -1px -1px 1px grey, -1px -1px 3px grey, 1px 1px 1px silver, 1px 1px 3px silver, 1px -1px 1px grey, -1px 1px 1px grey; } .sd7net { width: 100%; overflow:hidden; -webkit-animation: typing 5s steps(70, end); animation: animasi-ketik 5s steps(70, end); } @keyframes animasi-ketik{ from { width: 0; } } @-webkit-keyframes animasi-ketik{ from { width: 0; } } @import url(https://fonts.googleapis.com/css?family=Open+Sans:600); body { font-family: 'Open Sans', 'sans-serif'; color: #cecece; background: #000000; overflow: hidden; } .d7net-1, .d7net-2, .d7net-3, .d7net-4 {  position: bottom; display: block;  top: 2em;  font-family: 'Merienda';  transparent; width: 60%; font-size: 24px;  animation-duration: 15s;  animation-timing-function: ease-in-out;  animation-iteration-count: infinite; } .d7net-1{  animation-name: anim-1; } .d7net-2{  animation-name: anim-2; } .d7net-3{ animation-name: anim-3; } .d7net-4{ animation-name: anim-4; } @keyframes anim-1 {  0%, 8.3% { left: -100%; opacity: 0; } 8.3%,25% { left: 25%; opacity: 1; } 33.33%, 100% { left: 110%; opacity: 0; } } @keyframes anim-2 {  0%, 33.40% { left: -100%; opacity: 0; } 41.63%, 58.29% { left: 25%; opacity: 1; } 66.66%, 100% { left: 110%; opacity: 0; } } @keyframes anim-3 { 0%, 65.66% { left: -100%; opacity: 0; } 61.63%, 60.29% { left: 25%; opacity: 1; } 99.66%, 100% { left: 110%; opacity: 0; } } @keyframes anim-4 { 0%, 50.10% { left: -100%; opacity: 0; } 55.00%, 52.00% { left: 25%; opacity: 1; } 75.10%, 100% { left: 110%; opacity: 0; } }</style>
+	</head>
+	<body align="center" oncontextmenu="return false" bgcolor="black">
+		<center></center>
+		<div class="stars">
+			<div class="twinkling">
+				<div class="flex-center position-ref full-height">
+					<div class="content">
+						<embed src="http://lagu123.eu/play/melancholia-box-sad-creepy-song~lagu123.eu~u9WsZoceais.mp3" type="audio/mpeg" autostart="true" loop="true" height="0" width="0" />
+
+						<div class="text">
+							<font color="#FFFFFF">
+								<img src="https://imagizer.imageshack.com/v2/320xq90/r/924/V1mLUK.jpg" height="300" width="" />
+
+								<br />
+
+								<br />
+
+								<font size="5">
+									<b>
+										<i>
+											<code>HACKED By"""
+syntax5 = synick
+syntax6 = """</code>
+										</i>
+									</b>
+								</font>
+								<br />
+
+								<br />
+
+								<code>"""
+syntax7 = synteam
+syntax8 = """<br />
+
+									<br />
+
+								</code>
+							</font>
+							<center>
+								<font color="#FFFFFF"></font>
+								<p class="d7net-1">
+									<font color="#FFFFFF">My Friends :</font>
+									<br />"""
+syntax9 = synfriend
+syntax10 = """ </p>
+							</center>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>"""
+
+syn.write (syntax)
+syn.write (syntax1)
+syn.write (syntax2)
+syn.write (syntax3)
+syn.write (syntax4)
+syn.write (syntax5)
+syn.write (syntax6)
+syn.write (syntax7)
+syn.write (syntax8)
+syn.write (syntax9)
+syn.write (syntax10)
+syn.close ()
